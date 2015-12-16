5bf165f55e7aae7d5405c917f0457cea126a5c8e
Joao Paulo <joaopaulo@joaos-macbook-pro.local>
2015-12-15 23:23:20 -0200

    commit inicial

--- TaskManager/ViewController.swift
+++ TaskManager/ViewController.swift
@@ -8,8 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var taskNameTextField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var tasks = [String]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,38 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func addTask() {
+        
+        if taskNameTextField.text == "" {
+            return
+        }
+        
+        tasks.append(taskNameTextField.text!)
+        taskNameTextField.text = ""
+        taskNameTextField.resignFirstResponder()
+        tableView.reloadData()
+        
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return tasks.count
+        
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
+        configureCell(cell, atIndexPath: indexPath)
+        
+        return cell
+    }
+    
+    func configureCell(cell: UITableViewCell, atIndexPath indexPath: NSIndexPath) {
+        let task = tasks[indexPath.row]
+        cell.textLabel?.text = task
+    }
+    
 }
 
